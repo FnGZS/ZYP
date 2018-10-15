8d898aa09d5be648e5503a39fdf590a95fe7ae64
scott <3524300975@qq.com>
2018-10-15 18:22:31 +0800

    10-15

--- pages/user/user.js
+++ pages/user/user.js
@@ -7,22 +7,52 @@ Page({
    */
   data: {
     GradualNum: 0,
-    footSrc2: '../../images/首页.png',
-    footSrc3: '../../images/投票.png',
-    footSrc4: '../../images/时事.png',
-    footSrc5: '../../images/我的.png'
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  Navigation: function (event) {
-    var that = this;
-    app.Navigation(event, that);
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     wx.setStorageSync('GradualNum', 0);
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
-
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
