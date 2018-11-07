7322a8ef23a8cedcccb4aa33635e3fd271dad5e8
26324636 <37332537+26324636@users.noreply.github.com>
2018-11-07 19:39:32 +0800

    关于我们 校历

---
+++ pages/calendar/calendar.js
@@ -0,0 +1,68 @@
+const url = require('../../config.js')
+const sendAjax = require('../../utils/sendAjax.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    URL: url.host,
+    calendar:null
+  },
+  onLoad: function (options) {
+    this.getCalendar();
+  },
+
+  getCalendar:function(){
+    var that = this;
+    wx.request({
+      method: 'GET',
+      url: this.data.URL + '/calendar',
+      data: {
+        actionId: that.data.id,
+        studentId: that.data.studentId
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(res)
+        that.setData({
+          calendar: res.data.calendar
+        })
+      }
+    })
+  },
+  //图片预览
+  previewImage: function (e) {
+    console.log(e)
+    var pic = e.currentTarget.dataset.pic;
+    var pics = [];
+    pics.push(pic)
+    wx.previewImage({
+      current: pic, // 当前显示图片的http链接   
+      urls: pics // 需要预览的图片http链接列表   
+    })
+  },
+  onReady: function () {
+    
+  },
+  onShow: function () {
+    
+  },
+  onHide: function () {
+    
+  },
+  onUnload: function () {
+    
+  },
+  onPullDownRefresh: function () {
+    
+  },
+  onReachBottom: function () {
+    
+  },
+  onShareAppMessage: function () {
+    
+  }
+})
